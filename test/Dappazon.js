f5ca11235ed32abce20cb23eee77490c27654684
Mamitiana <mamitiana@fulldigits.com>
2023-01-21 15:53:49 +0300

    Pre 55:08 [ht] Formatting the code to have the listing item as variable

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -4,6 +4,16 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
+// Global constants for listing an item...
+const ID = 1;
+const NAME = "Shoes";
+const CATEGORY = "Clothing";
+const IMAGE =
+  "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg";
+const COST = tokens(1);
+const RATING = 4;
+const STOCK = 5;
+
 describe("Dappazon", () => {
   let dappazon;
   let deployer, buyer;
@@ -22,10 +32,11 @@ describe("Dappazon", () => {
 
   describe("Listing", () => {
     let transaction;
+
     beforeEach(async () => {
       transaction = await dappazon
         .connect(deployer)
-        .list(1, "NAME001", "CATEGORY001", "IMAGE001", 1, 4, 5);
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
       await transaction.wait();
     });
     it("Returns item attributes", async () => {
