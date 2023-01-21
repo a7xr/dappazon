068154ed5db28a217eddaf4e9da2fd29b0e18f94
Mamitiana <mamitiana@fulldigits.com>
2023-01-21 21:30:08 +0300

    Pre 2:24:17 Click on product is going to show the image of the product only ATM

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,18 +1,22 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Rating from './Rating'
+import Rating from "./Rating";
 
-import close from '../assets/close.svg'
+import close from "../assets/close.svg";
 
 const Product = ({ item, provider, account, dappazon, togglePop }) => {
-
   return (
     <div className="product">
-
-    </div >
+      <div className="product__details">
+        <div className="product__image">
+          <img src={item.image} alt="Product" />
+        </div>
+        <div className="product__overview"></div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Product;
