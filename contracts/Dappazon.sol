2a8b0ebbeade7ce35e07d128908648271e05c883
Mamitiana <mamitiana@fulldigits.com>
2023-01-21 15:33:37 +0300

    [Notes] Created the struct Item

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -4,11 +4,30 @@ pragma solidity ^0.8.9;
 contract Dappazon {
     address public owner;
 
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
     constructor() {
         owner = msg.sender;
     }
 
     // List products
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public {}
 
     // Buy products
 
