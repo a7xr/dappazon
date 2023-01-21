d3dfbe2aec7e13c42ed51add4f0210a8dd24a673
Mamitiana <mamitiana@fulldigits.com>
2023-01-21 14:41:12 +0300

    Pre 18:02 First test is OK

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,13 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 describe("Dappazon", () => {
-
-})
+  it("has a name", async () => {
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    dappazon = await Dappazon.deploy();
+    expect(await dappazon.name()).to.equal("Dappazon");
+  });
+});
