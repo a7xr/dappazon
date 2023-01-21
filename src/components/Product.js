e0062df3e3c88b20a857c583b48372bd5b992e0b
Mamitiana <mamitiana@fulldigits.com>
2023-01-21 21:31:44 +0300

    Pre 2:24:57 After click of product001, then going to show image, name, rating, price

--- src/components/Product.js
+++ src/components/Product.js
@@ -13,7 +13,19 @@ const Product = ({ item, provider, account, dappazon, togglePop }) => {
         <div className="product__image">
           <img src={item.image} alt="Product" />
         </div>
-        <div className="product__overview"></div>
+        <div className="product__overview">
+          <h1>{item.name}</h1>
+
+          <Rating value={item.rating} />
+
+          <hr />
+
+          <p>{item.address}</p>
+
+          <h2>{ethers.utils.formatUnits(item.cost.toString(), "ether")} ETH</h2>
+
+          <hr />
+        </div>
       </div>
     </div>
   );
