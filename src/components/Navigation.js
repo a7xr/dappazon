e72002c676a80f93f74d8f68fc47c653eccc0bd0
Mamitiana <mamitiana@fulldigits.com>
2023-01-21 20:12:26 +0300

    Pre 1:52:52 [nrs] Better String Dappazon in the Navigation_bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,13 @@
 import { ethers } from "ethers";
 
 const Navigation = ({ account, setAccount }) => {
-  return <nav>{account}</nav>;
+  return (
+    <nav>
+      <div className="nav__brand">
+        <h1>Dappazon</h1>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
