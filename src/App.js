9679aaa155d0d989f70add34c6929ce321a4b3fb
Mamitiana <mamitiana@fulldigits.com>
2023-01-21 19:50:13 +0300

    Pre 1:46:52 [nrs] Test usage of 'useEffect' in 'nrs'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './components/Navigation'
-import Section from './components/Section'
-import Product from './components/Product'
+import Navigation from "./components/Navigation";
+import Section from "./components/Section";
+import Product from "./components/Product";
 
 // ABIs
-import Dappazon from './abis/Dappazon.json'
+import Dappazon from "./abis/Dappazon.json";
 
 // Config
-import config from './config.json'
+import config from "./config.json";
 
 function App() {
+  const loadBlockchainData = async () => {
+    console.log("Loading");
+  };
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
 
   return (
     <div>
-
-      <h2>Welcome to Dappazon</h2>
-
+      <h2>Welcome to Dappazon !</h2>
     </div>
   );
 }
