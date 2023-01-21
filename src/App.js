cab44e5689966df8fa596d8869b1d8947d033ccf
Mamitiana <mamitiana@fulldigits.com>
2023-01-21 20:41:58 +0300

    Pre 2:5:28 [nrs] Connecting the frontEnd to the contract_dappazon we just deployed

--- src/App.js
+++ src/App.js
@@ -13,11 +13,24 @@ import Dappazon from "./abis/Dappazon.json";
 import config from "./config.json";
 
 function App() {
+  const [provider, setProvider] = useState(null);
   const [account, setAccount] = useState(null);
 
   const loadBlockchainData = async () => {
     // Connect to Blockchain
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    const network = await provider.getNetwork();
+    console.log(network);
+
     // Connect to Smart Contracts
+    const dappazon = new ethers.Contract(
+      "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      Dappazon,
+      provider
+    );
+
     // Load products
   };
   useEffect(() => {
